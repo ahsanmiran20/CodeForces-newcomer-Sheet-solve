3caf67adf07c8ae6370c9702b2ae7827381e91ca
Ahsan Miran <163660812+ahsanmiran20@users.noreply.github.com>
2025-02-12 14:28:26 +0600

    Create N - Numbers Histogram.cpp

---
+++ N - Numbers Histogram.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,i,j;
+    char symble;
+    scanf("%c",&symble);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x);
+        for(j=0;j<x;j++)
+            printf("%c",symble);
+        printf("\n");
+    }
+}
